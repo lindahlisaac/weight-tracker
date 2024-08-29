a4c047d76a53fe82a10cf826bd82b1b2b6903e1c
Isaac Lindahl <lindahl.isaac@gmail.com>
2024-08-28 20:26:39 -0700

    List is working, adds to screen on individual rows

--- src/app/pages/BaseLayout.jsx
+++ src/app/pages/BaseLayout.jsx
@@ -4,55 +4,69 @@ import Header from '../components/Header';
 import '../components/MenuBar.scss'
 import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
-import {useState} from 'react';
+import { useState } from 'react';
 import WeightEntryList from '../components/WeightEntryList';
 import WeightEntry from '../components/WeightEntry';
 
+function getFormattedDate() {
+    var date = new Date()
+    var year = date.getFullYear()
+    var month = date.getMonth()
+    var day = date.getDay()
+    if (month < 10) {
+        month = '0' + month
+    }
+    if (day < 10) {
+        day = '0' + day
+    }
+
+    return year + '-' + month + '-' + day
+}
+
 const weightEntry = {
-    weight: '170',
-    date: ''
+    weight: '',
+    date: '',
+    id: ''
 }
 
 const BaseLayout = () => {
 
     const [weightInput, setWeightInput] = useState()
-    const [weightEntryList, setWeightEntryList] = useState([
-       
-    ])
-  
-    function addWeightEntry(weight, date) {
+    const [weightEntryList, setWeightEntryList] = useState([])
+    const [entryId, setEntryId] = useState(0)
 
-        console.log({weight})
-        let newWeightEntryList
-        weightEntryList.push({weight, date})
-        newWeightEntryList = [...weightEntryList].slice()
-        setWeightEntryList(newWeightEntryList)
-        console.log(newWeightEntryList)
+    function addToList({ weightInput }) {
+        weightEntry.weight = weightInput
+        weightEntry.date = getFormattedDate()
+        console.log(weightEntry)
+        let tempList = weightEntryList.slice()
+        tempList.push({ weight: weightEntry.weight, date: weightEntry.date, id: entryId})
+        setWeightEntryList(tempList)
+        setEntryId(entryId + 1)
+        console.log(weightEntryList)
     }
 
     return (
         <>
-            <Header/>
-            <ul>{weightEntryList.map((weightEntry) => {
-                // console.log(weightEntry)
-                return (
-                    <WeightEntry weight={weightEntry.weight} date={weightEntry.date}/>
-                )
-                })}
+            <Header />
+            <ul>
+                {weightEntryList.map((weightEntry) => (
+                    <div>
+                        <li key={weightEntry.id}>{weightEntry.weight}     {weightEntry.date}</li>
+                    </div>
+                ))}
             </ul>
-            <div style={{position: "relative", backgroundColor: "black"}}>
+            <div style={{ position: "relative", backgroundColor: "black" }}>
                 <div className='menuBar'>
-                    <TextField id='weight-input-field' className='weightInputField' value={weightInput} onChange={(e) => setWeightInput(e.target.value, new Date().getDate.toDateString)}/>
+                    <TextField id='weight-input-field' className='weightInputField' value={weightInput} onChange={(e) => setWeightInput(e.target.value)} />
                     <Button className='addWeightButton' onClick={() => {
-                        // addWeightEntry({weightInput})
-                        console.log(document.getElementById("weight-input-field").value)
-                        addWeightEntry(new WeightEntry(document.getElementById("weight-input-field").value, new Date().toDateString)) 
-                        }}>Add Weight Entry</Button>
+                        addToList({ weightInput })
+                    }}>Add Weight Entry</Button>
                 </div>
             </div>
         </>
     );
- 
+
 }
- 
+
 export default BaseLayout;
