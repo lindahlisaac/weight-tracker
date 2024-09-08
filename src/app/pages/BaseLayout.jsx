fefe009b3929fd39966f8765ff904157c8f1b639
Isaac Lindahl <lindahl.isaac@gmail.com>
2024-09-08 09:15:30 -0700

    fix imports

--- src/app/pages/BaseLayout.jsx
+++ src/app/pages/BaseLayout.jsx
@@ -1,9 +1,6 @@
 "use client";
 import * as React from 'react';
 import WeightTrackerPage from './WeightTrackerPage';
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
 
 const BaseLayout = () => {
 
